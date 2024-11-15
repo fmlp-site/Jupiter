2514bd2bbd09a0dd97998920e8098cf5a4914f5c
fmlp-site <133589405+fmlp-site@users.noreply.github.com>
2024-11-15 12:48:07 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,19 @@
+const apiURLAdmin = "https://f1234-7a59.restdb.io/rest/acessoadmin?max=2";
+const headers = { "x-apikey": "671fb8e5b0c910dad08f7b43", "Content-Type": "application/json" };
 
+document.getElementById("btnLoginAdmin").addEventListener("click", async () => {
+  const usuario = document.getElementById("usuario").value;
+  const senha = document.getElementById("senhaAdm").value;
+
+  const response = await fetch(`${apiURLAdmin}?q={"usuario":"${usuario}","senha":"${senha}"}`, {
+    headers: headers,
+  });
+
+  const data = await response.json();
+  if (data.length > 0) {
+    localStorage.setItem("adminLogado", JSON.stringify(data[0]));
+    window.location.href = "centraldoadmin.html";
+  } else {
+    alert("Usuário ou senha inválidos!");
+  }
+});
